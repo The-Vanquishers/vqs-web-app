f0a66c80be8651cc6e2bacd2a82de85134ba0c7c
Rifat Islam <rifat.islam173@northsouth.edu>
2022-04-16 11:55:23 +0600

    fixed react-18 dom warning

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/es/integration/react";
 import "./index.css";
+import { createRoot } from "react-dom/client";
 import AppRouter from "./AppRouter";
 import { store, persistor } from "./store";
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <AppRouter />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
