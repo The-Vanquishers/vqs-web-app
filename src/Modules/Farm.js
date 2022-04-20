79d89d7544b577643e0382f34ceb1b4760fa32e0
saidurraahmaan <saidurraahmaan@gmail.com>
2022-04-20 06:38:47 +0600

    farm

--- src/Modules/Farm.js
+++ src/Modules/Farm.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { loginReducer } from "../reducers/login";
 import { empireReducer } from "../reducers/empire";
 import { connect } from 'react-redux';
-import { buildingNameToId, buildingPosition } from "../variables";
+import { buildingNameToId, buildingPosition,apiUrl } from "../variables";
 import { Grid, Typography } from "@mui/material";
 import Buildings from "../Assets/buildings.png";
+import axios from 'axios';
 
 
 const Farm = ({login,empire}) => {
@@ -12,6 +13,16 @@ const Farm = ({login,empire}) => {
   const farm = buildingNameToId["Farm"];
   const level = empire.buildings.filter((item) => item.buildingId === farm)[0].leve;
 
+  useEffect(() => {
+    const fetchHourProduction = async() => {
+      const { data } = await axios.get(`${apiUrl}/building/${farm}/${level+1}`, {
+        headers: { token: login.token, empireId: empire.empireId },
+      });
+      console.log(data);
+    }
+    fetchHourProduction();
+  }, [])
+  
   return (
     <>
       <Grid container spacing={2} sx={{ my: 3 }}>
@@ -34,16 +45,55 @@ const Farm = ({login,empire}) => {
           <Typography variant="h5" component="h6">
             Farm (Level {level})
           </Typography>
-          <Typography>This is farm that's produce food.</Typography>
-        </Grid>
-      </Grid>
-      <Grid container spacing={2} sx={{ my: 3 }}>
-        <Grid item xs={9}>
-          <Typography variant="h5" component="h6">
-            Farm (Level {level})
+          <Typography variant="body">
+            This is farm that's produce food.
           </Typography>
         </Grid>
       </Grid>
+      <Grid container spacing={2} sx={{ my: 1 }}>
+        <Grid item xs={5}>
+          <Typography variant="h6" component="h6">
+            Farm (Level {level})
+          </Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="h6" component="h6">
+            Units per hour
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h6" component="h6">
+            Units per hour at level {level + 1}
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} sx={{ my: 1 }}>
+        <Grid item xs={5}>
+          <Typography variant="body2">Base production</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="body2">Units per hour</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="body2">
+            Units per hour at level {level + 1}
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} sx={{ my: 1 }}>
+        <Grid item xs={5}>
+          <Typography variant="body2">Current production</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="body2">Units per hour</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="body2">
+            Units per hour at level {level + 1}
+          </Typography>
+        </Grid>
+      </Grid>
+      
     </>
   );
 }
